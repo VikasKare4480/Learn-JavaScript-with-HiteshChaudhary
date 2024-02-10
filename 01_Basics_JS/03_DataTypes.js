dd685a4e7e94fb747450d73bdbde52d2c144b34b
Vikas Kare <91896131+VikasKare4480@users.noreply.github.com>
2024-02-10 15:11:05 +0000

    DataTypes Done

---
+++ 01_Basics_JS/03_DataTypes.js
@@ -0,0 +1,30 @@
+
+let companyName = "Google";
+console.log(typeof companyName); // string
+
+companyName = 'Coditas';
+console.log(typeof companyName); // string
+
+companyName =  `Microsoft`;
+console.log(typeof companyName); // string 
+
+let currentYear = 2024;
+console.log(typeof currentYear); // number
+
+currentYear = 2024n;
+console.log(typeof currentYear); // bigint
+
+const CGPA = 8.95;
+console.log(typeof CGPA); // number
+
+const dataFetched = false;
+console.log(typeof dataFetched); // boolean
+
+let holdNothing;
+console.log(typeof holdNothing); // undefined 
+
+let holdsNullData = null;
+console.log(typeof holdsNullData); // object
+
+
+
