6cf1f51d8a3daf5f95e8f320a41286c58c4b97c3
Vikas Kare <91896131+VikasKare4480@users.noreply.github.com>
2024-02-27 18:06:20 +0000

    Iterators In JS Done

--- 03_Iterators.js/Map.js
+++ 03_Iterators.js/Map.js
@@ -30,6 +30,52 @@ const numbersArray_ = [1, 2, 3, 4,  5, 6, 7, 8, 9, 10];
 const newNumbersArray = numbersArray_.map((element) => element * 10).map((element) => element + 1).filter((element) => element > 50);
 console.log(newNumbersArray);
 
+const array1 = [1, 2, 3, 4,  5, 6, 7, 8, 9, 10];
 
+let initialValue = 100;
+const sumOfArrayElements = array1.reduce(
+    (accumlator, currentElement) => {
 
+        console.log(accumlator + currentElement);
+        return accumlator + currentElement;
+    }, initialValue // by default value is 0 because it is undefined in reduce function
+);
+console.log(sumOfArrayElements);
 
+const array2 = [11, 22, 33, 44,  55, 66]; // 
+
+let initialValue2 = 0;
+const sumOfArrayElements2 = array2.reduce(
+
+    function(acc, currValue) {
+
+        return acc + currValue;
+    }
+);
+
+console.log(sumOfArrayElements2);
+
+const products = [
+    {
+      name: "Headphones",
+      price: 49.99,
+      brand: "Sony",
+    },
+    {
+      name: "Laptop",
+      price: 799.99,
+      brand: "Dell",
+    },
+    {
+      name: "Smartwatch",
+      price: 249.99,
+      brand: "Fitbit",
+    },
+  ];
+  
+
+const getCartPrice = products.reduce(
+    (sum , currentElement) => sum + currentElement.price, 0
+);
+
+console.log(`getCartPrice ${getCartPrice}`);
